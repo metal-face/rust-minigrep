44427f3ae62a35812e950fc9c70d0702ac022a65
metal-face <captain@pirateradio.boats>
2022-12-24 08:40:31 -0500

    process from stdlib used to unwrap Result<>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // takes args passed in from cmd-line, collects them and places them in vector.
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
