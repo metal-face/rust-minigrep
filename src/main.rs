91805a369ff0828108b7bc1c6c949a02c089d1ef
metal-face <captain@pirateradio.boats>
2022-12-24 08:16:51 -0500

    no args handled in constructor

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("WE NEED MORE POWAAA!! (not enough arguments)");
+        }
+        
         let query = args[1].clone();
         let file_path = args[2].clone();
 
