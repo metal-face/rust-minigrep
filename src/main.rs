40aec1c2097fb6e5a1f5a7fab42d7f3e6da4ff96
metal-face <captain@pirateradio.boats>
2022-12-24 08:08:28 -0500

    function replaced by constructor

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,3 @@ impl Config {
         Config { query, file_path }
     }
 }
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
