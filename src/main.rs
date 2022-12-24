621622ef188351c0b4a64aaf45d3c4da1d921621
metal-face <captain@pirateradio.boats>
2022-12-24 07:40:01 -0500

    to prepare for separation, separate method written

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ fn main() {
     // takes args passed in from cmd-line, collects them and places them in vector.
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -15,4 +14,11 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
